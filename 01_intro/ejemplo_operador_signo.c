bf79ba1c031ba882dcf61dee192df7bcbd368bda
Songeo <sonia.gmcl@gmail.com>
2017-01-29 11:52:17 -0600

    ejemplo operador

---
+++ 01_intro/ejemplo_operador_signo.c
@@ -0,0 +1,11 @@
+
+//Asignación con operador + signo:
+
+#include<stdio.h>
+
+main(){
+    double variable = 1;
+    printf("variable %f\n", variable);
+    variable /= 2; //ie, variable = variable /2;
+    printf("variable: %0.4e\n",variable);
+}
