b3d72442ccb6b4b55e8392b835a3d3eca1912b25
Songeo <sonia.gmcl@gmail.com>
2017-01-29 14:56:53 -0600

    ejemplos 1 y 2 de clase sbado

---
+++ 01_intro/ejemplo_double_int.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	int var_int;
+	double var_double = -5191.485
+	print("variable double = %0.5f\n", var_double);
+	var_int = var_double;
+	print("variable int = %d\n", var_int);
+	print("int + double= %f\n", var_int + var_double);
+
+}
--- 01_intro/ejemplo_operador_signo.c
+++ 01_intro/ejemplo_operador_signo.c
@@ -6,6 +6,6 @@
 main(){
     double variable = 1;
     printf("variable %f\n", variable);
-    variable /= 2; //ie, variable = variable /2;
+    variable /= 2; //ie división, variable = variable /2;
     printf("variable: %0.4e\n",variable);
 }
